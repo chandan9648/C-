9236944fdd800f8482416f1d5a5257348f2b8bf6
Chandan Chaudhary <chandankkumar156@gmail.com>
2026-03-01 12:30:40 +0530

    Refactor Program.cs to include Main method and update assembly information

--- obj/Debug/net10.0/C#.AssemblyInfo.cs
+++ obj/Debug/net10.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe6c8ba0d118d9d2da105bba0296ea00d3c715c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a9538eb86d7b9e3678ba7f64ecb98e4e170b00")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
